7038a138f6a1c29777a896de96af73a6f167263f
EulerssSon <alfy.kerolous@gmail.com>
2024-03-10 05:24:44 -0700

    added base model

---
+++ models/base_model.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""This module contains the base model the parent for
+all models in airBnB clone app"""
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """This class is the parent class for all models in the airBnB clone app"""
+
+    def __init__(self) -> None:
+        """This method initializes the base model
+
+        Attributes:
+            id (str): the unique id of the model
+            created_at (datetime): the time the model was created
+            updated_at (datetime): initially the same as created_at,
+            but updated when the model is modified
+        """
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self) -> str:
+        """This method returns the string representation of the model"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self) -> None:
+        """This method updates the updated_at attribute to the current time"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self) -> dict:
+        """This method returns a dictionary representation of the model"""
+        model_dict = self.__dict__.copy()
+        model_dict["__class__"] = self.__class__.__name__
+        model_dict["created_at"] = self.created_at.isoformat()
+        model_dict["updated_at"] = self.updated_at.isoformat()
+        return model_dict
